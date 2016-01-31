2296154f0bdf52382bdf52b0a0be2966894d8d9c
Uras Mutlu <urasmutlu@urass-mac.local>
2016-01-31 04:12:35 +0200

    V1 of the app is implemented

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingLabel.hidden = false
+        stopButton.hidden = false
+        //TODO: Record audio
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingLabel.hidden = true
+        stopButton.hidden = true
+    }
 }
 
